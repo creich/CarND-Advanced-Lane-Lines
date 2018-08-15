64ddba3262595a7a15e5142bd9f6b3a2d6afafa6
creich <creich@linux.com>
2018-08-15 14:51:28 +0200

    * changed calibration data for perspective transformation * added basic lane pixel detection (simple sliding window) * added .gitignore

--- lanefinder.py
+++ lanefinder.py
@@ -204,22 +204,28 @@ def createThresholdedBinaryImage(image):
 
     return combined_binary
 
-
-def calcPerspactiveTransformMatrix():
-    image = mpimg.imread('test_images/straight_lines1.jpg')
-
-    print(image.shape)
-    y_max, x_max = image.shape[0], image.shape[1]
+#TODO clean up! most stuff is not really needed here. only for debugging...
+def calcPerspactiveTransformMatrix(cameraMatrix, distortionCoeffs):
     #                  top_left    top_right  bot_left    bot_right
-    src = np.float32([[580, 460], [700, 460], [300, 660], [1000, 660]])
-    dst = np.float32([[400, 460], [900, 460], [400, 660], [900, 660]])
+    # from undistorted straight_lines2
+    src = np.float32([[585, 460], [702, 460], [310, 660], [1000, 660]])
+    # from orig straight_lines1
+    #src = np.float32([[580, 460], [700, 460], [300, 660], [1000, 660]])
+    dst = np.float32([[400, 200], [900, 200], [400, 660], [900, 660]])
+    #dst = np.float32([[400, 460], [900, 460], [400, 660], [900, 660]])
     # use cv2.getPerspectiveTransform() to get M, the transform matrix
     M = cv2.getPerspectiveTransform(src, dst)
 
     if DEBUG:
+        image = mpimg.imread('test_images/straight_lines1.jpg')
+        image = undistortImage(image, cameraMatrix, distortionCoeffs)
+
+        print(image.shape)
+        y_max, x_max = image.shape[0], image.shape[1]
         warped = cv2.warpPerspective(image, M, (x_max, y_max), flags=cv2.INTER_LINEAR)
 
         image2 = mpimg.imread('test_images/straight_lines2.jpg')
+        image2 = undistortImage(image2, cameraMatrix, distortionCoeffs)
         warped2 = cv2.warpPerspective(image2, M, (x_max, y_max), flags=cv2.INTER_LINEAR)
 
         f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(24, 9))
@@ -238,9 +244,131 @@ def calcPerspactiveTransformMatrix():
         ax4.set_title('warped2', fontsize=15)
 
         plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-        #plt.show()
+        plt.show()
 
+    #TODO check if return of (x_max, y_max) makes sense here!
     return M, (x_max, y_max)
+
+
+def find_lane_pixels(binary_warped):
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
+    # Create an output image to draw on and visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]//2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # HYPERPARAMETERS
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+
+    # Set height of windows - based on nwindows above and image shape
+    window_height = np.int(binary_warped.shape[0]//nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated later for each window in nwindows
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        ### TO-DO: Find the four below boundaries of the window ###
+        win_xleft_low = leftx_current - margin  # Update this
+        win_xleft_high = leftx_current + margin  # Update this
+        win_xright_low = rightx_current - margin  # Update this
+        win_xright_high = rightx_current + margin  # Update this
+
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),
+        (win_xleft_high,win_y_high),(0, 1, 0), 3)
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),
+        (win_xright_high,win_y_high),(0, 1, 0), 3)
+
+        ### TO-DO: Identify the nonzero pixels in x and y within the window ###
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+        (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+        (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+
+        ### TO-DO: If you found > minpix pixels, recenter next window ###
+        ### (`right` or `leftx_current`) on their mean position ###
+        #pass # Remove this when you add your function
+        if len(good_left_inds) > minpix:
+            mean_left = np.mean(nonzerox[good_left_inds])
+            leftx_current = np.int(mean_left)
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+
+    # Concatenate the arrays of indices (previously was a list of lists of pixels)
+    try:
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+    except ValueError:
+        # Avoids an error if the above is not implemented fully
+        pass
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    return leftx, lefty, rightx, righty, out_img
+
+
+def fit_polynomial(binary_warped):
+    # Find our lane pixels first
+    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
+
+    ### TO-DO: Fit a second order polynomial to each using `np.polyfit` ###
+    # why are x and y parameters swapped?
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    try:
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    except TypeError:
+        # Avoids an error if `left` and `right_fit` are still none or incorrect
+        print('The function failed to fit a line!')
+        left_fitx = 1*ploty**2 + 1*ploty
+        right_fitx = 1*ploty**2 + 1*ploty
+
+    ## Visualization ##
+    # Colors in the left and right lane regions (1 on binary images whereas it should be 255 on colored images)
+    out_img[lefty, leftx] = [1, 0, 0]
+    out_img[righty, rightx] = [0, 0, 1]
+
+    # Plots the left and right polynomials on the lane lines
+    #plt.plot(left_fitx, ploty, color='yellow')
+    #plt.plot(right_fitx, ploty, color='yellow')
+
+    #TODO draw lines into the image instead of returning line coordinates
+    return out_img, left_fitx, right_fitx, ploty
+    #return out_img
 
 
 def pipeline(image, cameraMatrix, distortionCoeffs, transformationMatrix, image_size):
@@ -253,28 +381,60 @@ def pipeline(image, cameraMatrix, distortionCoeffs, transformationMatrix, image_
     #TODO crop image first, to save computation power
     binary_image = createThresholdedBinaryImage(image)
 
+    ## Apply a perspective transform to rectify binary image ("birds-eye view").
+    binary_warped = cv2.warpPerspective(binary_image, transformationMatrix, image_size, flags=cv2.INTER_LINEAR)
+
+    ## Detect lane pixels and fit to find the lane boundary.
+    #leftx, lefty, rightx, righty, out_image = find_lane_pixels(binary_warped)
+    out_image, left_fitx, right_fitx, ploty = fit_polynomial(binary_warped)
+    #out_image = fit_polynomial(binary_warped)
+
+    #FIXME fulfill this missing requirement
+    ## Determine the curvature of the lane and vehicle position with respect to center.
+
     #==========
     #FIXME remove this suff
-    #f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(24, 9))
-    #f.tight_layout()
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(24, 9))
+    f.tight_layout()
 
-    #ax1.imshow(gray_combined)
-    #ax1.set_title('gray_combined', fontsize=15)
+    ax1.imshow(image)
+    ax1.set_title('image', fontsize=15)
 
-    #plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-    #plt.show()
+    ax2.imshow(binary_image)
+    ax2.set_title('binary_image', fontsize=15)
+
+    ax3.imshow(binary_warped)
+    ax3.set_title('binary_warped', fontsize=15)
+
+    ax4.plot(left_fitx, ploty, color='yellow')
+    ax4.plot(right_fitx, ploty, color='yellow')
+    ax4.imshow(out_image)
+    ax4.set_title('out_image', fontsize=15)
+
+    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
+    #plt.imshow(binary_warped)
+    #plt.imshow(out_img)
+
+    plt.show()
     #==========
-
-    ## Apply a perspective transform to rectify binary image ("birds-eye view").
-    warped = cv2.warpPerspective(binary_image, transformationMatrix, image_size, flags=cv2.INTER_LINEAR)
 
 
 def main():
     ## Compute the camera calibration matrix and distortion coefficients given a set of chessboard images.
     ret, cameraMatrix, distortionCoeffs, rvecs, tvecs = calibrateCamera()
+    
+    if DEBUG:
+        image = mpimg.imread('test_images/straight_lines1.jpg')
+        image = undistortImage(image, cameraMatrix, distortionCoeffs)
+        mpimg.imsave('output_images/straight_lines1_undistorted.jpg', image)
+
+        image = mpimg.imread('test_images/straight_lines2.jpg')
+        image = undistortImage(image, cameraMatrix, distortionCoeffs)
+        mpimg.imsave('output_images/straight_lines2_undistorted.jpg', image)
 
     # calculate transformation matrix
-    transformationMatrix, image_size = calcPerspactiveTransformMatrix()
+    transformationMatrix, image_size = calcPerspactiveTransformMatrix(cameraMatrix, distortionCoeffs)
 
     # Make a list of calibration images
     images = glob.glob('test_images/test*.jpg')
